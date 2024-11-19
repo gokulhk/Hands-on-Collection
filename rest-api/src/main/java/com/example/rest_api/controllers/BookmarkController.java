c98af6e050448aefc61c66f9292ddba26d793272
Gokulnath <gokul.chandiran@anywhere.co>
2024-11-19 19:04:06 +0530

    feat: added api versioning

--- rest-api/src/main/java/com/example/rest_api/controllers/BookmarkController.java
+++ rest-api/src/main/java/com/example/rest_api/controllers/BookmarkController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/bookmarks")
+@RequestMapping("/api/v1/bookmarks")
 @Log
 public class BookmarkController {
 
