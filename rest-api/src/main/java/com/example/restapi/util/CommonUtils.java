16065b9b73cadc14d415ffc7b3f6bc72cdd511cc
Gokulnath <gokul.chandiran@anywhere.co>
2025-04-24 13:42:34 +0530

    refactor: remove unused code

--- rest-api/src/main/java/com/example/restapi/util/CommonUtils.java
+++ rest-api/src/main/java/com/example/restapi/util/CommonUtils.java
@@ -1,15 +1,11 @@
 package com.example.restapi.util;
 
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.java.Log;
-import org.springframework.data.domain.Sort;
-import org.springframework.validation.BindingResult;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.logging.Level;
+import lombok.extern.java.Log;
+import org.springframework.validation.BindingResult;
 
 @Log
 public class CommonUtils {
@@ -39,34 +35,5 @@ public class CommonUtils {
       log.log(Level.SEVERE, "exception processing bindingResult errors: ", e);
     }
     return new HashMap<>();
-  }
-
-  public static Sort.Direction getSortingOrder(HttpServletRequest request) {
-    return Optional.ofNullable(request.getParameter("sort"))
-        .filter(val -> val.contains(":desc"))
-        .map(val -> Sort.Direction.DESC)
-        .orElse(Sort.Direction.ASC);
-  }
-
-  public static Optional<String> getSortByParam(HttpServletRequest request) {
-    return Optional.ofNullable(request.getParameter("sort"))
-        .filter(val -> !val.isBlank())
-        .map(val -> val.split(":")[0]);
-  }
-
-  public static int getOffsetParam(
-      HttpServletRequest request, int minAllowed, int maxAllowed, int defaultOffset) {
-    return Optional.ofNullable(request.getParameter("offset"))
-        .map(Integer::parseInt)
-        .filter(val -> val >= minAllowed && val <= maxAllowed)
-        .orElse(defaultOffset);
-  }
-
-  public static int getLimitParam(
-      HttpServletRequest request, int minAllowed, int maxAllowed, int defaultLimit) {
-    return Optional.ofNullable(request.getParameter("limit"))
-        .map(Integer::parseInt)
-        .filter(val -> val >= minAllowed && val <= maxAllowed)
-        .orElse(defaultLimit);
   }
 }
